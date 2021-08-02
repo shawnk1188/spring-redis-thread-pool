f5e7e72abe424e0f5c1efdc5ed05d160f1b9c1c8
shawnk1188 <shawngit1188@gmail.com>
2021-08-01 23:59:23 -0500

    Update Readme.cmd

--- Readme.cmd
+++ Readme.cmd
@@ -1,1 +1,21 @@
 
+Spring Data Redis Application Implementing Thread Pool to Recieve Message Queues.
+
+Application Recieves Messages via rest api and pushes them to queue.
+
+
+Build instructions (any 3rd party requirements and how to generally get them setup on either linux or mac) -- docker-compose or something similar is suggested
+
+docker-compose up --build will start the application at port 8888
+
+Usage instructions (i.e. samples to actually show how it works)
+
+PostMan Requests:
+Headers: content-type "application/json"
+Method: POST
+
+{
+    "channelName":"logMessage",
+    "logMessage":"sending in a log message no info log"
+}
+
